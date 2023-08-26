5293fe8853883d1fb356f010456655892e2f22dc
Danielle Lindblom <danielle.lindblom@gmail.com>
2023-08-26 14:34:41 -0600

    removed search results component - not needed

--- src/App.js
+++ src/App.js
@@ -5,20 +5,15 @@ import React, { useState } from 'react';
 import ActivityList from './ActivityList/ActivityList';
 import MyActivities from './MyActivities/MyActivities';
 import SearchBar from './SearchBar/SearchBar';
-import SearchResults from './SearchResults/SearchResults';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Select Your Camp Week Activities!</h1>
-      <header className="App-header">
-        
-        <ActivityList className="container"/>
-        <div className="container">
-        <SearchBar />
-        <SearchResults />
-        </div>
+      <SearchBar />
+      <header className="App-header">           
+        <ActivityList className="container"/>        
         <MyActivities className="container"/>
       </header>
     </div>
