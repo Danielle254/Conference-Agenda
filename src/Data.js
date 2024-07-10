43e93347b3107e92ab4714d71e7f9ed57dd4846c
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-10 12:58:02 -0600

    udpate data set with full conference agenda

--- src/Data.js
+++ src/Data.js
@@ -18,13 +18,13 @@
     location: "Ballroom C"
     },
     {
-    title: "Session 2",
+    title: "Women in Tech: How to Find Community",
     speaker: "Maria Morrison",
     timeHour: 14,
     timeMinute: 30,
     displayTime: "2:30pm",
     id: 3,
-    location: "Ballroom C" 
+    location: "Ballroom D" 
     },
     {
     title: "Lessons in Accessibility",
@@ -33,7 +33,79 @@
     timeMinute: 30,
     displayTime: "1:30pm",
     id: 4,
+    location: "Main Ballroom"    
+    },
+    {
+    title: "Community Networking Time",
+    speaker: "All are Welcome",
+    timeHour: 15,
+    timeMinute: 0,
+    displayTime: "3:00pm",
+    id: 5,
     location: "Ballroom D"    
+    },
+    {
+    title: "Community Networking Time",
+    speaker: "All are Welcome",
+    timeHour: 19,
+    timeMinute: 0,
+    displayTime: "7:00pm",
+    id: 6,
+    location: "Main Ballroom"    
+    },
+    {
+    title: "Basic Design Principles for Developers",
+    speaker: "Natalie Allan",
+    timeHour: 15,
+    timeMinute: 30,
+    displayTime: "3:30pm",
+    id: 7,
+    location: "Ballroom D"    
+    },
+    {
+    title: "Teamwork Makes the Dream Work",
+    speaker: "Stephanie Landon",
+    timeHour: 14,
+    timeMinute: 30,
+    displayTime: "2:30pm",
+    id: 8,
+    location: "Ballroom C"    
+    },
+    {
+    title: "Managing Your Manager: How to Stay Sane",
+    speaker: "Emma Nash",
+    timeHour: 16,
+    timeMinute: 30,
+    displayTime: "4:30pm",
+    id: 9,
+    location: "Ballroom B"    
+    },
+    {
+    title: "Mental Health Matters: How to Start Small",
+    speaker: "Angela Peake",
+    timeHour: 16,
+    timeMinute: 30,
+    displayTime: "4:30pm",
+    id: 10,
+    location: "Ballroom D"    
+    },
+    {
+    title: "Closing: Amp up Your Impact",
+    speaker: "Hannah Cameron",
+    timeHour: 19,
+    timeMinute: 30,
+    displayTime: "7:30pm",
+    id: 11,
+    location: "Main Ballroom"    
+    },
+    {
+    title: "Big Tech vs Small Startup: How to Find Your Fit",
+    speaker: "Hannah Cameron",
+    timeHour: 18,
+    timeMinute: 30,
+    displayTime: "6:30pm",
+    id: 12,
+    location: "Ballroom B"    
     }
 ];
 
